74f3bdef86424656b12134c1a5b5868e93034ec2
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-04-17 12:28:38 -0300

    commit

---
+++ DataStructures/src/binaryTree/TreeNode.java
@@ -0,0 +1,12 @@
+package binaryTree;
+
+public class TreeNode {
+    
+    private TreeNode left;
+    private TreeNode right;
+    private int data;
+
+    public TreeNode(){
+        this.data = data;
+    }
+}
