c5f2a4f2e0acd8c86282f157e72f1d6410369e2c
RodManzella <manzellateixeira@gmail.com>
2023-10-07 22:16:24 -0300

    commit

---
+++ DataStructures/src/Graphs/DFSTargetSum/GraphNode.java
@@ -0,0 +1,27 @@
+package Graphs.DFSTargetSum;
+
+public class GraphNode {
+    public static final int BRANCO = 0;
+    public static final int CINZA = 1;
+    public static final int PRETO = 2;
+
+    private int value ;
+    private int index;
+
+    public GraphNode(int value, int index) {
+        this.value= value;
+        this.index = index;
+    }
+
+    public int getName() {
+        return value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
